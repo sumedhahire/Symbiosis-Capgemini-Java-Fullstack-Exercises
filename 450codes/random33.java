af08897daae022e868cf9035a2ac9106a1565437
Sumedh ahire <94281812+sumedhahire@users.noreply.github.com>
2024-07-03 18:56:49 +0530

    random 33

---
+++ 450codes/random33.java
@@ -0,0 +1,10 @@
+//How to Generate Random Numbers in Java
+
+import java.lang.Math;
+class random33{
+	
+	public static void main(String... args){
+
+		System.out.println("random number:"+(int)(Math.random()*100));
+	}
+}
