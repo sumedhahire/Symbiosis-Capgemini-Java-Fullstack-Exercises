1c8c08df27504ede9b9fa75a79a196f5a432e0e9
Sumedh ahire <94281812+sumedhahire@users.noreply.github.com>
2024-07-04 17:47:30 +0530

    Create swap5.java

---
+++ 450codes/simple/swap5.java
@@ -0,0 +1,14 @@
+public class swap5 {
+    
+        public static void main(String[] args) {
+            int a=3;
+            int b=5;
+            
+            b=b*a;
+            a=b/a;
+            b=b/a;
+            
+            System.out.println(a+" "+b);
+            
+        }
+}
