b65e6c272fe20f5b270cc4af5597a2af8d29aaae
Sumedh ahire <94281812+sumedhahire@users.noreply.github.com>
2024-07-04 17:55:00 +0530

    Create three39_52.java

---
+++ 450codes/if-else/three39_52.java
@@ -0,0 +1,20 @@
+public class three39_52 {
+    
+        public static void main(String[] args) {
+            int a=3;
+            int b=5;
+            int c=1;
+            
+            if(a>b && a>c){
+                System.out.println(a);
+            }else{
+                if(b>c)
+                    System.out.println(b);
+                else
+                    System.out.println(c);
+            }
+            
+            
+            
+        }
+}
