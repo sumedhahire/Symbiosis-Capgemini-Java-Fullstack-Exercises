afac4dd346c9dcdd69d596e8e24e4c565d5a0a5b
Sumedh ahire <94281812+sumedhahire@users.noreply.github.com>
2024-07-04 18:04:30 +0530

    Create upper60.java

---
+++ 450codes/if-else/upper60.java
@@ -0,0 +1,16 @@
+public class upper60 {
+    
+        public static void main(String[] args) {
+            //X 88
+            //A 65
+            //a 97
+            //x 120
+            int a=args[0].charAt(0);
+            if(a>=97 && a<=120 )
+                System.out.println("uppercase");
+            else if(a>=65 && a<=88)
+                System.out.println("lowercase");
+            
+            
+        }
+}
