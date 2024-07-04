b327e59c1f2506462546c6dcd3b59725d43789d0
Sumedh ahire <94281812+sumedhahire@users.noreply.github.com>
2024-07-04 17:50:27 +0530

    Create positive44.java

---
+++ 450codes/if-else/positive44.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+        public static void main(String[] args) {
+            int a=Integer.valueOf(args[0]);
+            if(a>=0)
+                System.out.print("positive");
+            else
+                System.out.print("negative");
+          
+            
+        }
+}
